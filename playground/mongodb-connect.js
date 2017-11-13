1969e1485d0fa16dfb2bb11c4e7a12fd0c36764d
deeCodeMonkey <deidra108@gmail.com>
2017-11-13 11:56:23 -0700

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,45 @@
+﻿//const MongoClient = require('mongodb').MongoClient;
+
+//can create new object IDs on the fly
+const { MongoClient, ObjectID } = require('mongodb');
+
+////create new instance of obj id
+//var obj = new ObjectID();
+//console.log(obj);
+
+//ES6 destructure. Can create new variables from an object's properties
+//var user = { name: 'Deidra', age: 38 };
+//var { name } = user;
+//console.log(name);
+
+//TodoApp database is created here. Callback if err, else db to write commands
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('unable to connect Mongodb: ' + err);
+    }
+    console.log('Connected to Mongodb server.');
+
+    //db.collection('Todos').insertOne({
+    //    text: 'Something to do',
+    //    completed: false
+    //}, (err, result) => {
+    //    if (err) {
+    //        return console.log('Unable to insert Todo. ', err);
+    //    }
+    //    //ops will store all inserted. Undefined for filter and indent 2
+    //    console.log(JSON.stringify(result.ops, undefined, 2));
+    //});
+
+    //db.collection('Users').insertOne({
+    //    name: 'Deidra',
+    //    age: 38,
+    //    location: 'Denver'
+    //}, (err, result) => {
+    //    if (err) {
+    //        return console.log('Unable to insert.', err);
+    //    }
+    //    console.log(result.ops[0]._id.getTimestamp());
+    //});
+
+    db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,37 @@
+﻿//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('unable to connect Mongodb: ' + err);
+    }
+    console.log('Connected to Mongodb server.');
+
+    //db.collection('Todos').find({
+    //    _id: new ObjectID("5a09d7cf4d33ec382481b704")
+    //}).toArray().then((docs) => {
+    //    console.log('Todos');
+    //    console.log(JSON.stringify(docs, undefined, 2));
+    //}, (err) => {
+    //    console.log('Unable to fetch todos', err);
+    //});
+
+    //db.collection('Todos').find().count().then((count) => {
+    //    console.log(`Todos count: ${count}`);
+    //    console.log(JSON.stringify(count, undefined, 2));
+    //}, (err) => {
+    //    console.log('Unable to fetch', err);
+    //});
+
+    db.collection('Users').find({ name: 'Deidra' }).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    });
+
+
+
+
+
+    //db.close();
+});
