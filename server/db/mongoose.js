9d5c224e00b90bbe080e0c860eb6c41efb18ff63
deeCodeMonkey <deidra108@gmail.com>
2017-11-15 15:00:11 -0700

    heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 ﻿var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+//connecting mongoose to mongodb
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = mongoose;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
-﻿const express = require('express');
-const bodyParser = require('body-parser');
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
-const { mongoose } = require('./db/mongoose');
-const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
-//set up middleware. send json to express
 app.use(bodyParser.json());
 
-//to set up routes
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
+
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
@@ -22,6 +23,34 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('started port 3000');
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+});
+
+module.exports = { app };
